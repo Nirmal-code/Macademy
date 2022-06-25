511543676f184d4b615346b963fb42cb0aeadcb0
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-06-25 17:35:20 -0400

    Add files via upload

---
+++ src/pages/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Components/header/header'
+
+
+export default function HomePage(){
+    return(
+        <div>
+            <Header />
+            <h1>Welcome! This is the Home page!!</h1>
+
+        </div>
+    );
+};
