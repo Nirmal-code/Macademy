3da9cdba13a17bf71c427c811921c3a4bb32ac26
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-06-25 17:32:42 -0400

    Update planner.js

--- src/pages/planner/planner.js
+++ src/pages/planner/planner.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../Components/header/header'
 
 export default function PlannerPage(){
     return(
         <div>
+            <Header />
             <h1>This is the Planner Page!</h1>
         </div>
     );
