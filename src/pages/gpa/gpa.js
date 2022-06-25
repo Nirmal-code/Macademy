ca0465a87ac8172d3b64c9d6b0eb1600865c940a
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-06-25 17:34:09 -0400

    Update gpa.js

--- src/pages/gpa/gpa.js
+++ src/pages/gpa/gpa.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from '../Components/header/header';
+
 
 export default function GPAPage(){
     return(
         <div>
+            <Header />
             <h1>This is the GPA page!</h1>
         </div>
     );
