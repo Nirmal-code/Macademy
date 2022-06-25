5ff48c03574fc1012a6b23cdb1206a75f8da5f3b
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-06-25 18:21:22 -0400

    Update footer.js

---
+++ src/pages/Components/footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './footer.css';
+
+
+export default function Footer(){
+    return(
+        <footer id="footer">
+            <h4 id="copyright">Developed by Nirmal Chaudhari. &copy; 2022.All rights reserved.</h4>
+        </footer>
+    );
+};
