b528d6cc6bab46f226aaa057b0b54b8749ea6881
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-06-27 14:25:10 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import Header from './Components/header/header'
+import './index.css'
 
 
 export default function IndexPage(){
     return(
         <div>
-            <h1>Welcome! This is the Home page!!</h1>
-            <a href='./planner/planner/'>Planner Page</a>
-            <br></br>
-            <a href='./timer/timer/'>Timer Page</a>
-            <br></br>
-            <a href='./gpa/gpa/'>GPA Page</a>
+            <Header />
+            <div id="indexelements">
+                <h1>Welcome! This is the Index page!!</h1>
+                <a href="./home/home/">Home</a>
+                <br></br>
+                <a href='./planner/planner/'>Planner Page</a>
+                <br></br>
+                <a href='./timer/timer/'>Timer Page</a>
+                <br></br>
+                <a href='./gpa/gpa/'>GPA Page</a>
+            </div>
 
         </div>
     );
