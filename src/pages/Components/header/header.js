2ae575258d034e15a4b7c25103c063663bbead45
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-08-02 15:38:11 -0400

    Update header.js

--- src/pages/Components/header/header.js
+++ src/pages/Components/header/header.js
@@ -15,7 +15,7 @@ export default function Header(){
                     <li><img id='logo' src={logo}></img></li>
                     <li><Link to='../../home/home/' id='title'><h1>MACademy</h1></Link></li>
                     <li><Link to='../../home/home/' class='active' id="menu-item"><img id='icon' src={home_icon}></img><h3>HOME</h3></Link></li>
-                    <li><Link to='../../planner/planner/' class='active' id="menu-item"><img id='icon' src={calendar_icon}></img><h3> PLANNER/h3></Link></li>
+                    <li><Link to='../../planner/planner/' class='active' id="menu-item"><img id='icon' src={calendar_icon}></img><h3> PLANNER</h3></Link></li>
                     <li><Link to='../../timer/timer/' class='active' id="menu-item"><img id='icon' src={clock_icon}></img><h3>TIMER</h3></Link></li>
                     <li><Link to='../../gpa/gpa/' class='active' id="menu-item"><img id='icon' src={gpa_icon}></img><h3>GPA</h3></Link></li>
                 </ul>
