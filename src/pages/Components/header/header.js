693a628ffeda91140e8c20cbf492661560725b4c
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-08-02 15:31:07 -0400

    Update header.js

--- src/pages/Components/header/header.js
+++ src/pages/Components/header/header.js
@@ -14,10 +14,10 @@ export default function Header(){
                 <ul>
                     <li><img id='logo' src={logo}></img></li>
                     <li><Link to='../../home/home/' id='title'><h1>MACademy</h1></Link></li>
-                    <li><Link to='../../home/home/' id="menu-item"><img id='icon' src={home_icon}></img><h3>Home</h3></Link></li>
-                    <li><Link to='../../planner/planner/' id="menu-item"><img id='icon' src={calendar_icon}></img><h3> Planner</h3></Link></li>
-                    <li><Link to='../../timer/timer/' id="menu-item"><img id='icon' src={clock_icon}></img><h3>Timer</h3></Link></li>
-                    <li><Link to='../../gpa/gpa/' id="menu-item"><img id='icon' src={gpa_icon}></img><h3>GPA</h3></Link></li>
+                    <li><Link to='../../home/home/' class='active' id="menu-item"><img id='icon' src={home_icon}></img><h3>HOME</h3></Link></li>
+                    <li><Link to='../../planner/planner/' class='active' id="menu-item"><img id='icon' src={calendar_icon}></img><h3> PLANNER/h3></Link></li>
+                    <li><Link to='../../timer/timer/' class='active' id="menu-item"><img id='icon' src={clock_icon}></img><h3>TIMER</h3></Link></li>
+                    <li><Link to='../../gpa/gpa/' class='active' id="menu-item"><img id='icon' src={gpa_icon}></img><h3>GPA</h3></Link></li>
                 </ul>
 
 
