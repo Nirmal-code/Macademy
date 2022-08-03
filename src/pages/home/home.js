08558fc93f4bf224d06da50183773a75d830fdd3
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-08-02 21:06:23 -0400

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '../Components/header/header'
+import './home.css';
 
 
 export default function HomePage(){
     return(
         <div>
             <Header />
+            <title>Macademy-Home</title>
             <h1>Welcome! This is the Home page!!</h1>
+            <Footer />
 
         </div>
     );
