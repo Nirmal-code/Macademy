2a1cb84924108af3e5826e42842615a397b216b6
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-08-02 21:07:38 -0400

    Update planner.js

--- src/pages/planner/planner.js
+++ src/pages/planner/planner.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from '../Components/header/header'
+import './planner.css';
 
 export default function PlannerPage(){
     return(
         <div>
             <Header />
+            <title>Macademy-Planner</title>
             <h1>This is the Planner Page!</h1>
+            <Footer />
         </div>
     );
 };
