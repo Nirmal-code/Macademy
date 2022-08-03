fc823274e82f612e13f9aaec878ad90d367ed0c8
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-08-03 12:12:01 -0400

    Update header.js

--- src/pages/Components/header/header.js
+++ src/pages/Components/header/header.js
@@ -8,6 +8,10 @@ import clock_icon from '../../../images/clock_icon.png';
 import gpa_icon from '../../../images/gpa_icon.png';
 
 export default function Header(){
+    
+    const date=new Date()
+    var current_date=date.getMonth()+'/'+date.getDay()+'/'+date.getFullYear()
+    
     return(
         <div id="header-wrap">
             <navbar id="main-header">
@@ -18,6 +22,8 @@ export default function Header(){
                     <li><Link to='../../planner/planner/' class='active' id="menu-item"><img id='icon' src={calendar_icon}></img><h3> PLANNER</h3></Link></li>
                     <li><Link to='../../timer/timer/' class='active' id="menu-item"><img id='icon' src={clock_icon}></img><h3>TIMER</h3></Link></li>
                     <li><Link to='../../gpa/gpa/' class='active' id="menu-item"><img id='icon' src={gpa_icon}></img><h3>GPA</h3></Link></li>
+                    <li id='time'>{current_date} </li>
+
                 </ul>
 
 
