7218fc0366c96a82b46526030c377e9d5c06895c
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-08-04 15:49:24 -0400

    Update header.js

--- src/pages/Components/header/header.js
+++ src/pages/Components/header/header.js
@@ -7,28 +7,30 @@ import calendar_icon from '../../../images/calendar_icon.png';
 import clock_icon from '../../../images/clock_icon.png';
 import gpa_icon from '../../../images/gpa_icon.png';
 
+
+
 export default function Header(){
-    
+
     const date=new Date()
     var current_date=date.getMonth()+'/'+date.getDay()+'/'+date.getFullYear()
-    
+
     return(
+        
         <div id="header-wrap">
             <navbar id="main-header">
-                <ul>
+                <ul id='logoname'>
                     <li><img id='logo' src={logo}></img></li>
                     <li><Link to='../../home/home/' id='title'><h1>MACademy</h1></Link></li>
+                </ul>
+                <ul id='menu-holder'>
                     <li><Link to='../../home/home/' class='active' id="menu-item"><img id='icon' src={home_icon}></img><h3>HOME</h3></Link></li>
                     <li><Link to='../../planner/planner/' class='active' id="menu-item"><img id='icon' src={calendar_icon}></img><h3> PLANNER</h3></Link></li>
                     <li><Link to='../../timer/timer/' class='active' id="menu-item"><img id='icon' src={clock_icon}></img><h3>TIMER</h3></Link></li>
                     <li><Link to='../../gpa/gpa/' class='active' id="menu-item"><img id='icon' src={gpa_icon}></img><h3>GPA</h3></Link></li>
-                    <li id='time'>{current_date} </li>
-
+                    <li id='time'>{current_date}</li>
                 </ul>
-
-
-
             </navbar>
         </div>
     );
+
 };
