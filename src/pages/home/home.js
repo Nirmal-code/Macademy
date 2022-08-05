e2548fed528fd38afc7e940ac0eb2d00e5585922
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-08-05 13:14:54 -0400

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from '../Components/header/header';
 import Footer from '../Components/footer/footer';
 import './home.css';
+import mainpic from '../../images/home_page_image.png';
+import image2 from '../../images/home_image2.png';
 
 
 export default function HomePage(){
@@ -9,7 +11,12 @@ export default function HomePage(){
         <div id='home'>
             <Header />
             <title>Macademy-Home</title>
-            <h1>Welcome! This is the Home page!!</h1>
+            <div id='caption-wrap'>
+                <h1 id='main-caption'>An effective studying tool for students to use!</h1>
+                <h4 id='secondary-caption'>MACademy is a tool students of all ages can use to develop better studying habits.</h4>
+            </div>
+            <img id='main-picture' src={mainpic}></img>
+            <img id='image2' src={image2}></img>
             <Footer />
         </div>
     );
