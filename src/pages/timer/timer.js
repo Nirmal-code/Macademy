c18ae30407e62d862288be2a075bed1bb26b1564
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-08-15 17:16:00 -0400

    Update timer.js

--- src/pages/timer/timer.js
+++ src/pages/timer/timer.js
@@ -26,8 +26,51 @@ export default function TimerPage(){
         isStopped=true;
     }
 
+    function getData2(){
+        var b_hour=parseInt(document.getElementById('b_hours').value);
+        var b_minute=parseInt(document.getElementById('b_mins').value);
+        var b_second=parseInt(document.getElementById('b_secs').value);
+
+        isPaused=false;
+        isStopped=false;
+
+        
+        x=setInterval(() => {
+            if (!isPaused){
+                b_second=b_second-1
+
+                if (b_second<=0){
+                    b_minute=b_minute-1
+
+                    if (b_minute<0){
+                        b_hour=b_hour-1
+                        b_minute=59
+                    }
+                    b_second=59;
+                }
+                
+                document.getElementById("reciever").innerHTML = b_hour + "h "
+                        + b_minute + "m " + b_second + "s ";
+                
+                if (b_hour<0){
+                    clearInterval(x);
+                    document.getElementById("reciever").innerHTML = "Stop Procrastinating now :)";
+                    getData()
+                }
+
+                if (isStopped==true) {
+                    clearInterval(x);
+                    document.getElementById("reciever").innerHTML = "EXPIRED";
+                }
+
+            }
+        }, 1000);
+
+    }
+
 
     function getData(){
+
 
         var hour=parseInt(document.getElementById('hours').value);
         var minute=parseInt(document.getElementById('mins').value);
@@ -43,6 +86,7 @@ export default function TimerPage(){
 
                 if (second<=0){
                     minute=minute-1
+
                     if (minute<0){
                         hour=hour-1
                         minute=59
@@ -53,9 +97,11 @@ export default function TimerPage(){
                 document.getElementById("reciever").innerHTML = hour + "h "
                         + minute + "m " + second + "s ";
                 
-                if (hour==0 && minute==0 && second==0){
+                if (hour<0){
                     clearInterval(x);
-                    document.getElementById("reciever").innerHTML = "EXPIRED";
+                    document.getElementById("reciever").innerHTML = "Enjoy Your Break :)";
+                    getData2();
+
                 }
 
                 if (isStopped==true) {
@@ -71,19 +117,24 @@ export default function TimerPage(){
     return(
         <div id="timer">
             <Header />
-            <title>Macademy-Interval Studying</title>
-
-            <p id='reciever'></p>
+            <title>Macademy-Interval Studying</title>            
 
             <form id='countdown_info' onsubmit="return false">
-                <input id='hours' type='number'></input>h
+                Set Studying Time: <input id='hours' type='number'></input>h
                 <input id='mins' type='number'></input>min
-                <input id='secs' type='number'></input>sec
+                <input id='secs' type='number'></input>sec<br></br>
+                Set Break Time:<input id='b_hours' type='number'></input>h
+                <input id='b_mins' type='number'></input>min
+                <input id='b_secs' type='number'></input>sec
             </form>
+
             <button id="starter_btn" onClick={getData}>Start</button>
             <button id="pause_btn" onClick={pause}>Pause</button>
             <button id="resume_btn" onClick={resume}>Resume</button>
             <button id="stop_btn" onClick={stop}>Stop</button>
+
+            <p id='reciever'></p>
+
 
 
             <Footer />
