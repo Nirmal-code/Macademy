c55edd472aef2d9f0ce47e5fcd12ff22a18cd816
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-08-21 19:13:09 -0400

    Update planner.js

--- src/pages/planner/planner.js
+++ src/pages/planner/planner.js
@@ -5,11 +5,16 @@ import './planner.css'
 
 export default function PlannerPage(){
     return(
-        <div id="planner">
+        <div class='plannerjs' id="planner">
             <Header />
             <title>Macademy-Planner</title>
             <h1>This is the Planner Page!</h1>
+            <ul id='timer_id'>
+                <li id='nb_reciever'>Timer Not Started: </li>
+                <li id='reciever'>_h _min _sec</li>
+            </ul>
             <Footer />
         </div>
     );
 };
+
