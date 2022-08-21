9105f06509c4479a40fdf93af25e88c20814be14
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-08-21 19:12:24 -0400

    Update gpa.js

--- src/pages/gpa/gpa.js
+++ src/pages/gpa/gpa.js
@@ -6,9 +6,13 @@ import './gpa.css';
 
 export default function GPAPage(){
     return(
-        <div id="gpa">
+        <div class='gpajs'id="gpa">
             <Header />
             <title>Macademy-GPA</title>
+            <ul id='timer_id'>
+                    <li id='nb_reciever'>Timer Not Started: </li>
+                    <li id='reciever'>_h _min _sec</li>
+            </ul>
             <h1>This is the GPA page!</h1>
             <Footer />
         </div>
