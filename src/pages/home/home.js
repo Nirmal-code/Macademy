dfec31671dee6096c4a4117f1b50b2e2ad427f6a
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-08-23 18:19:55 -0400

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -9,14 +9,16 @@ import gpa_icon from '../../images/gpa_icon.png';
 
 
 export default function HomePage(){
-    document.addEventListener("orientationchange", function(event){
-        switch(window.orientation) 
-        {  
-            case 90: case -90:
-                break; 
-            default:
-        }
-    });
+    if (typeof window !=='undefined'){
+        document.addEventListener("orientationchange", function(event){
+            switch(window.orientation) 
+            {  
+                case 90: case -90:
+                    break; 
+                default:
+            }
+        });
+    };
 
     return(
         <div class='homejs' id='home'>
