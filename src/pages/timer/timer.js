cf36acd8df25cd127a78a6ded29af85caa94f74f
Nirmal Chaudhari <69442620+Nirmal-code@users.noreply.github.com>
2022-08-23 21:06:52 -0400

    Update timer.js

--- src/pages/timer/timer.js
+++ src/pages/timer/timer.js
@@ -39,13 +39,11 @@ export default function TimerPage(){
 
 
 
-    function getData2(){
+    function getData2(hour, minute, second, b_hour, b_minute, b_second){
 
-        var b_hour=parseInt(document.getElementById('b_hours').value);
-        var b_minute=parseInt(document.getElementById('b_mins').value);
-        var b_second=parseInt(document.getElementById('b_secs').value);
-
-
+        var b_hour_store=b_hour;
+        var b_minute_store=b_minute;
+        var b_second_store=b_second;
         
         x=setInterval(() => {
 
@@ -77,7 +75,10 @@ export default function TimerPage(){
                     clearInterval(x);
                     document.getElementById("reciever").innerHTML = "Stop Procrastinating now :)";
                     document.getElementById("nb_reciever").innerHTML="Study Time!"
-                    getData()
+                    b_hour=b_hour_store;
+                    b_minute=b_minute_store;
+
+                    getData(hour, minute, second, b_hour_store, b_minute_store, b_second_store)
                 }
 
                 if (window.isStopped==true) {
@@ -92,12 +93,26 @@ export default function TimerPage(){
 
     }
 
-
-    function getData(){
-
+    function userData(){
         var hour=parseInt(document.getElementById('hours').value);
         var minute=parseInt(document.getElementById('mins').value);
-        var second=parseInt(document.getElementById('secs').value);        
+        var second=parseInt(document.getElementById('secs').value);   
+        
+        var b_hour=parseInt(document.getElementById('b_hours').value);
+        var b_minute=parseInt(document.getElementById('b_mins').value);
+        var b_second=parseInt(document.getElementById('b_secs').value);
+
+        
+
+        getData(hour, minute, second, b_hour, b_minute, b_second)
+    }
+
+
+    function getData(hour, minute, second, b_hour, b_minute, b_second){
+
+        var hour_store=hour;
+        var minute_store=minute;
+        var second_store=second;
 
 
         x=setInterval(() => {
@@ -130,8 +145,12 @@ export default function TimerPage(){
                 if (hour<0){
                     clearInterval(x);
                     document.getElementById("reciever").innerHTML = "Enjoy Your Break :)";
-                    document.getElementById("nb_reciever").innerHTML="Break Time!"
-                    getData2();
+                    document.getElementById("nb_reciever").innerHTML="Break Time!";
+                    hour=hour_store;
+                    minute=minute_store;
+                    second=second_store;
+
+                    getData2(hour_store, minute_store, second_store, b_hour,b_minute,b_second);
 
                 }
 
@@ -164,7 +183,7 @@ export default function TimerPage(){
             <p id='reciever'>__h __min __sec</p>
 
             <div id='all_btn'>
-                <button id="starter_btn" onClick={getData}>Start</button>
+                <button id="starter_btn" onClick={userData}>Start</button>
                 <button id="pause_btn" onClick={pause}>Pause</button>
                 <button id="resume_btn" onClick={resume}>Resume</button>
                 <button id="stop_btn" onClick={stop}>Stop</button>
